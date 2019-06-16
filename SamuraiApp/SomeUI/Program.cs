7b35c627c680cff2431277b08feaad09e0ce6e57
Vatan Soni <vatansoni89@gmail.com>
2019-06-17 01:07:54 +0530

    //InsertSamurai();            // InsertMultipleSamurai();
               // InsertSumuraiAndBattleAtOneShot();
    
               // QueryAndUpdateBattle_Disconnected();
    
               // DeletemanySamurai();

--- SamuraiApp/SomeUI/Program.cs
+++ SamuraiApp/SomeUI/Program.cs
@@ -10,9 +10,64 @@ namespace SomeUI
 {
     class Program
     {
+        public static SamuraiContext Context { get; set; } = new SamuraiContext();
+
         static void Main(string[] args)
         {
-            InsertSamurai();
+            //InsertSamurai();
+           // InsertMultipleSamurai();
+
+           // InsertSumuraiAndBattleAtOneShot();
+
+           // QueryAndUpdateBattle_Disconnected();
+
+           // DeletemanySamurai();
+        }
+
+        private static void DeletemanySamurai()
+        {
+            var samuS = Context.Samurais.Where(s => s.Name.Contains("samu"));
+
+            Context.Samurais.RemoveRange(samuS);
+            Context.SaveChanges();
+        }
+
+        private static void QueryAndUpdateBattle_Disconnected()
+        {
+            var battle = Context.Battles.FirstOrDefault();
+            battle.EndDate = DateTime.Now.AddYears(-20);
+
+            using (var newContext = new SamuraiContext())
+            {
+                newContext.Battles.Update(battle);
+                newContext.SaveChanges();
+            }
+
+        }
+
+        private static void InsertSumuraiAndBattleAtOneShot()
+        {
+            var samu = new Samurai { Name = "samu1" };
+            var battle = new Battle { Name="Panipat",StartDate=DateTime.Now.AddYears(-100), EndDate = DateTime.Now.AddYears(-50) };
+
+            using (var context = new SamuraiContext())
+            {
+                //added in ef core 2
+                context.AddRange(samu, battle);
+                context.SaveChanges();
+            }
+        }
+
+        private static void InsertMultipleSamurai()
+        {
+            var samurai1 = new Samurai { Name = "samu1" };
+            var samurai2 = new Samurai { Name = "samu2" };
+
+            using (var context = new SamuraiContext())
+            {
+                context.Samurais.AddRange(samurai1,samurai2);
+                context.SaveChanges();
+            }
         }
 
         private static void InsertSamurai()
